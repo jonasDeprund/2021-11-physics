e101bae1315a5b701ac68e9516b12637ff892a2e
Jonas Deprund <jonasdeprund@gmail.com>
2021-10-19 19:16:52 +0200

    wip balls

--- src/script.js
+++ src/script.js
@@ -165,16 +165,18 @@ renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
 const objectsToUpdate = [];
 
+const sphereGeometry = new THREE.SphereBufferGeometry(1, 20, 20);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+// Sphere
 const createSphere = (radius, position) => {
   // Three.js mesh
-  const mesh = new THREE.Mesh(
-    new THREE.SphereBufferGeometry(radius, 20, 20),
-    new THREE.MeshStandardMaterial({
-      metalness: 0.3,
-      roughness: 0.4,
-      envMap: environmentMapTexture,
-    })
-  );
+  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+  mesh.scale.set(radius, radius, radius);
   mesh.castShadow = true;
   mesh.position.copy(position);
   scene.add(mesh);
@@ -202,6 +204,8 @@ createSphere(0.5, {
   y: 3,
   z: 0,
 });
+
+// Box
 
 /**
  * Animate
