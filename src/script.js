cf9325eb1fcb8895f2f11c69874cf5ec6acc8999
Jonas Deprund <jonasdeprund@gmail.com>
2021-10-20 18:37:36 +0200

    box added

--- src/script.js
+++ src/script.js
@@ -7,6 +7,8 @@ import CANNON from 'cannon';
 /**
  * Debug
  */
+
+// Sphere
 const gui = new dat.GUI();
 const debugObject = {};
 
@@ -18,7 +20,24 @@ debugObject.createSphere = () => {
   });
 };
 
+// Box
+
+debugObject.createBox = () => {
+  createBox(
+    Math.random(),
+    Math.random(),
+    Math.random(),
+
+    {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    }
+  );
+};
+
 gui.add(debugObject, 'createSphere');
+gui.add(debugObject, 'createBox');
 
 /**
  * Base
@@ -206,6 +225,40 @@ createSphere(0.5, {
 });
 
 // Box
+const boxGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const boxMaterial = new THREE.MeshStandardMaterial({
+  metalness: 0.3,
+  roughness: 0.4,
+  envMap: environmentMapTexture,
+});
+
+const createBox = (width, height, depth, position) => {
+  // Three.js mesh
+  const mesh = new THREE.Mesh(boxGeometry, boxMaterial);
+  mesh.scale.set(width, height, depth);
+  mesh.castShadow = true;
+  mesh.position.copy(position);
+  scene.add(mesh);
+
+  // cannon js Body
+  const shape = new CANNON.Box(
+    new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5)
+  );
+  const body = new CANNON.Body({
+    mass: 1,
+    position: new CANNON.Vec3(0, 3, 0),
+    shape,
+    material: defaultMaterial,
+  });
+  body.position.copy(position);
+  world.addBody(body);
+
+  // Save objects to update
+  objectsToUpdate.push({
+    mesh: mesh,
+    body: body,
+  });
+};
 
 /**
  * Animate
@@ -223,6 +276,7 @@ const tick = () => {
 
   for (const object of objectsToUpdate) {
     object.mesh.position.copy(object.body.position);
+    object.mesh.quaternion.copy(object.body.quaternion);
   }
 
   // Update controls
